5221f7d6b71358d9485386e49462f1f7314b3368
Alessio Cassibba (X-Drum) <swapon@gmail.com>
2014-08-15 13:56:45 +0200

    minor cosmetics

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -43,85 +43,82 @@
 # Copyright 2014 Alessio Cassibba (X-Drum), unless otherwise noted.
 #
 class puppet::agent (
-	$servername = $puppet::params::puppetagent_servername,
-	$noop = $puppet::params::puppetagent_noop,
-	$cron = $puppet::params::puppetagent_cron,
-	$service = $puppet::params::puppetagent_service,
-	$pluginsync = $puppet::params::puppetagent_pluginsync,
-	$configtimeout = $puppet::params::puppetagent_configtimeout,
+  $servername = $puppet::params::puppetagent_servername,
+  $noop = $puppet::params::puppetagent_noop,
+  $cron = $puppet::params::puppetagent_cron,
+  $service = $puppet::params::puppetagent_service,
+  $pluginsync = $puppet::params::puppetagent_pluginsync,
+  $configtimeout = $puppet::params::puppetagent_configtimeout,
 ) inherits puppet::params {
 
-	case $::osfamily {
-		redhat: {
-			include yum::repo::puppetlabs
-		}
-	}
+  case $::osfamily {
+    redhat: {
+      include yum::repo::puppetlabs
+    }
+  }
 
-	package { $puppet::params::puppetagent_pkg:
-		ensure => installed,
-	}
-	file { $puppet::params::puppet_config:
-		ensure => present,
-		owner => $puppet::params::puppet_owner,
-		group => $puppet::params::puppet_group,
-		mode => '0644',
-	}
+  package { $puppet::params::puppetagent_pkg:
+    ensure => installed,
+  }
+  file { $puppet::params::puppet_config:
+  	ensure => present,
+  	owner  => $puppet::params::puppet_owner,
+  	group  => $puppet::params::puppet_group,
+  	mode   => '0644',
+  }
 
-	Ini_setting {
-		path => $puppet::params::puppet_config,
-		ensure => present,
-	}
-	ini_setting { 'puppetagent_servername':
-		section => 'agent',
-		setting => 'server',
-		value => "${servername}",
-	}
-	ini_setting { 'puppetagent_pluginsync':
-		section => 'agent',
-		setting => 'pluginsync',
-		value => "${pluginsync}",
-	}
-	ini_setting { 'puppetagent_noop':
-		section => 'agent',
-		setting => 'noop',
-		value => "${noop}",
-	}
-	ini_setting { 'puppetagent_configtimeout':
-		section => 'agent',
-		setting => 'configtimeout',
-		value => "${configtimeout}",
-	}
+  Ini_setting {
+  	path   => $puppet::params::puppet_config,
+  	ensure => present,
+  }
+  ini_setting { 'puppetagent_servername':
+    section => 'agent',
+    setting => 'server',
+    value   => "${servername}",
+  }
+  ini_setting { 'puppetagent_pluginsync':
+    section => 'agent',
+    setting => 'pluginsync',
+    value   => "${pluginsync}",
+  }
+  ini_setting { 'puppetagent_noop':
+    section => 'agent',
+    setting => 'noop',
+    value   => "${noop}",
+  }
+  ini_setting { 'puppetagent_configtimeout':
+    section => 'agent',
+    setting => 'configtimeout',
+    value   => "${configtimeout}",
+  }
 
-	if $service {
-		$service_enable = true
-		$service_ensure = running
-	}
-	else {
-		$service_enable = false
-		$service_ensure = stopped
-	}
-	service { $puppet::params::puppetagent_srv:
-		enable => $service_enable,
-		ensure => $service_ensure,
-		hasrestart => true,
-		hasstatus => true,
-		require => Package[$puppet::params::puppetagent_pkg],
-		subscribe => File[$puppet::params::puppet_config]
-	}
+  $service_ensure = $service ? {
+    true  => running,
+    false => stopped,
+  }
 
-    /*randomize cron runs..*/
-    $r1 = fqdn_rand(30)
-	$r2 = $r1+30
-	if $cron {
-		$cron_ensure = present
-	}
-	else {
-		$cron_ensure = absent
-	}
-	cron { 'puppetagent':
-		ensure => $cron_ensure,
-		command => "${puppet::params::puppet_prefix}/${puppet::params::puppetagent_croncommand} --configtimeout ${puppet::params::puppetagent_configtimeout} 2>&1 >/dev/null",
-		user => $puppet::params::puppet_owner,
-		minute => [$r1,$r2],
-	}
+  service { $puppet::params::puppetagent_srv:
+  	enable     => $service,
+  	ensure     => $service_ensure,
+  	hasrestart => true,
+  	hasstatus  => true,
+  	require    => Package[$puppet::params::puppetagent_pkg],
+  	subscribe  => File[$puppet::params::puppet_config]
+  }
+
+  /*randomize cron runs..*/
+  $r1 = fqdn_rand(30)
+  $r2 = $r1+30
+  if $cron {
+  	$cron_ensure = present
+  }
+  else {
+  	$cron_ensure = absent
+  }
+  cron { 'puppetagent':
+    ensure  => $cron_ensure,
+    command => "${puppet::params::puppet_prefix}/${puppet::params::puppetagent_croncommand} --configtimeout ${puppet::params::puppetagent_configtimeout} 2>&1 >/dev/null",
+    user    => $puppet::params::puppet_owner,
+    minute  => [$r1,$r2],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -14,48 +14,46 @@
 # Copyright 2014 Alessio Cassibba (X-Drum), unless otherwise noted.
 #
 class puppet::params {
-	$puppet_config = "/etc/puppet/puppet.conf"
-	$puppetmaster_standalone = true
-	$puppetmaster_autosign = false
-	$puppetmaster_servername = "${::fqdn}"
-	$puppetmaster_modulepath = '$confdir/modules:/usr/share/puppet/modules'
-	$puppetmaster_docroot = "/etc/puppet/rack"
-	$puppetmaster_cert = "/var/lib/puppet/ssl/certs/${puppetmaster_servername}.pem"
-	$puppetmaster_key = "/var/lib/puppet/ssl/private_keys/${puppetmaster_servername}.pem"
-	$puppetmaster_ca = "/var/lib/puppet/ssl/ca/ca_crt.pem"
-	$puppetmaster_crl = "/var/lib/puppet/ssl/ca/ca_crl.pem"
-	$puppetagent_servername = "change_me_to_puppet_server"
-	$puppetagent_noop = false
-	$puppetagent_cron = false
-	$puppetagent_service = true
-	$puppetagent_pluginsync = true
-	$puppetagent_configtimeout = '900'
-	$puppetagent_croncommand = "puppet agent --test"
+  $puppet_config = "/etc/puppet/puppet.conf"
+  $puppetmaster_standalone = true
+  $puppetmaster_autosign = false
+  $puppetmaster_servername = "${::fqdn}"
+  $puppetmaster_modulepath = '$confdir/modules:/usr/share/puppet/modules'
+  $puppetmaster_docroot = "/etc/puppet/rack"
+  $puppetmaster_cert = "/var/lib/puppet/ssl/certs/${puppetmaster_servername}.pem"
+  $puppetmaster_key = "/var/lib/puppet/ssl/private_keys/${puppetmaster_servername}.pem"
+  $puppetmaster_ca = "/var/lib/puppet/ssl/ca/ca_crt.pem"
+  $puppetmaster_crl = "/var/lib/puppet/ssl/ca/ca_crl.pem"
+  $puppetagent_servername = "change_me_to_puppet_server"
+  $puppetagent_noop = false
+  $puppetagent_cron = false
+  $puppetagent_service = true
+  $puppetagent_pluginsync = true
+  $puppetagent_configtimeout = '900'
+  $puppetagent_croncommand = "puppet agent --test"
 
-	case $::osfamily {
-		redhat: {
-			$puppet_owner = "root"
-			$puppet_group = "root"
-			$puppet_prefix = "/usr/bin"
-			$puppetagent_pkg = "puppet"
-			$puppetagent_srv = "puppet"
-			$puppetmaster_pkg = "puppet-server"
-			$puppetmaster_srv = "puppetmaster"
-			$rack_pkg = 'rubygem-rack'
-			$rack_config = '/usr/share/puppet/ext/rack/config.ru'
-			$passenger_root = '/usr/lib/ruby/gems/1.8/gems/passenger-3.0.21'
-		}
-		openbsd: {
-			$puppet_owner = "root"
-			$puppet_group = "wheel"
-			$puppet_prefix = "/usr/local/bin"
-			$puppetagent_pkg = "puppet"
-			$puppetagent_srv = "puppetd"
-		}
-		default: {
-			fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
-		}
-	}
+  case $::osfamily {
+  	redhat: {
+  	  $puppet_owner = "root"
+  	  $puppet_group = "root"
+  	  $puppet_prefix = "/usr/bin"
+  	  $puppetagent_pkg = "puppet"
+  	  $puppetagent_srv = "puppet"
+  	  $puppetmaster_pkg = "puppet-server"
+  	  $puppetmaster_srv = "puppetmaster"
+  	  $rack_pkg = 'rubygem-rack'
+  	  $rack_config = '/usr/share/puppet/ext/rack/config.ru'
+  	  $passenger_root = '/usr/lib/ruby/gems/1.8/gems/passenger-3.0.21'
+  	}
+  	openbsd: {
+  	  $puppet_owner = "root"
+  	  $puppet_group = "wheel"
+  	  $puppet_prefix = "/usr/local/bin"
+  	  $puppetagent_pkg = "puppet"
+  	  $puppetagent_srv = "puppetd"
+  	}
+  	default: {
+  	  fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+  	}
+  }
 }
-  
-
